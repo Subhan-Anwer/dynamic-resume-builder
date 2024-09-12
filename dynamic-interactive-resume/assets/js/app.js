d61ab7b1fa9a1b6343f53414a85a9e21c49fee7d
Subhan-Anwer <sheikhanwer30@gmail.com>
2024-09-13 01:30:09 +0500

    final commit

---
+++ dynamic-interactive-resume/assets/js/app.js
@@ -0,0 +1,46 @@
+const yourName = document.querySelector('#yourName');
+const userName = document.querySelector('#userName');
+const userProfession = document.querySelector('#userProfession');
+const yourProfession = document.querySelector('#yourProfession');
+const userAbout = document.querySelector('#userAbout');
+const yourAbout = document.querySelector('#yourAbout');
+const userPhone = document.querySelector('#userPhone');
+const yourPhone = document.querySelector('#yourPhone');
+const userEmail = document.querySelector('#userEmail');
+const yourEmail = document.querySelector('#yourEmail');
+const userAddress = document.querySelector('#userAddress');
+const yourAddress = document.querySelector('#yourAddress');
+const userSkill1 = document.querySelector('#userSkill1');
+const userSkill2 = document.querySelector('#userSkill2');
+const userSkill3 = document.querySelector('#userSkill3');
+const userSkill4 = document.querySelector('#userSkill4');
+const userSkill5 = document.querySelector('#userSkill5');
+const userSkill6 = document.querySelector('#userSkill6');
+const userSkill7 = document.querySelector('#userSkill7');
+const userCompany = document.querySelector('#userCompany');
+const userJobTitle = document.querySelector('#userJobTitle');
+const userJobDuration = document.querySelector('#userJobDuration');
+const userUni = document.querySelector('#userUni');
+const userUniDegree = document.querySelector('#userUniDegree');
+const userUniDuration = document.querySelector('#userUniDuration');
+const userSchool = document.querySelector('#userSchool');
+const userSchoolDegree = document.querySelector('#userSchoolDegree');
+const userSchoolDuration = document.querySelector('#userSchoolDuration');
+const userSoftSkill1 = document.querySelector('#userSoftSkill1');
+const userSoftSkill2 = document.querySelector('#userSoftSkill2');
+const userSoftSkill3 = document.querySelector('#userSoftSkill3');
+const userSoftSkill4 = document.querySelector('#userSoftSkill4');
+const userHobby1 = document.querySelector('#userHobby1');
+const userHobby2 = document.querySelector('#userHobby2');
+const userHobby3 = document.querySelector('#userHobby3');
+const userHobby4 = document.querySelector('#userHobby4');
+
+
+// button
+const submitBtn = document.querySelector('#submitBtn');
+
+
+submitBtn.addEventListener('click', () => {
+    yourName.innerText = userName.placeholder
+    // alert('Subhan')
+}) 
